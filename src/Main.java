29fbfca69697da53d47595795770660348573d5f
Bogdan622527474 <116581623+Bogdan622527474@users.noreply.github.com>
2022-10-24 19:54:52 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This task done by Bogdan Bachurskiy SE-19-1/9");
+        System.out.println("This task done by Bogdan Bachurskiy SE-19-1/9");//my name
         Date date = new Date();
-        System.out.println("Current date" +date);
+        System.out.println("Current date" +date);//date
     }
 }
