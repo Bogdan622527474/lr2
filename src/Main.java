19438f3d912b8513ae04b53267e68a9d3eb8a90f
Bogdan622527474 <bbacurskij@gmail.com>
2022-10-24 19:55:54 +0300

    return_to_start

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
-import java.util.Date;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("This task done by Bogdan Bachurskiy SE-19-1/9");
-        Date date = new Date();
-        System.out.println("Current date" +date);
+
     }
 }
